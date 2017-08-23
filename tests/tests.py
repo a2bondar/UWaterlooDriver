5674f94faac352cbb489b5a5640d505470163d6f
Anton B <anton.bnd@gmail.com>
2017-08-22 22:54:21 -0400

    Hardened the test for /foodservices/menu/ endpoint.

--- tests/tests.py
+++ tests/tests.py
@@ -7,14 +7,19 @@ def menu_keys():
     # Responsible for only returning the test data
     return ['date', 'outlets']
 
+@fixture
+def date_keys():
+    return ['year', 'week', 'start', 'end']
+
 @vcr.use_cassette('vcr_cassettes/foodservices_menu.yml', filter_query_parameters=['key'])
-def test_foodservices_manu(menu_keys):
+def test_foodservices_manu(menu_keys, date_keys):
     """Tests an API call to /foodservices/menu/ endpoint."""
 
     uw_driver = UW_Driver()
     response = uw_driver.foodservices_menu()
 
     assert isinstance(response, dict)
-    # Not testing subkeys because 'outlets' could be empty at certain times.
+    # Not testing subkeys in 'outlets' because it could be empty at certain times.
     assert set(menu_keys).issubset(response.keys()), "All keys should be in the response."
-
+    assert set(date_keys).issubset(response['date'].keys()), "All date keys should be present."
+    assert isinstance(response['outlets'], list)
